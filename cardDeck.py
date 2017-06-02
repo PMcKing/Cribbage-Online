4f32126a5f67da9ee45c4fcead14f3e7a40f3e07
Peter King <peterk1216@gmail.com>
2017-06-02 20:19:23 +0000

    fixed list error in player hands

--- cardDeck.py
+++ cardDeck.py
@@ -2,7 +2,8 @@ import requests
 from Card import Card
 
 deck = [None] * 3 #id, success, remaining
-
+p1Hand = [None] * 6
+p2Hand = [None] * 6
 def getDeck():
     #gets a new shuffled deck
     global deck
@@ -13,24 +14,27 @@ def getDeck():
     deck[1] = response.json()['success']
     deck[2] = response.json()['remaining']
     
-def dealhandplayer():
-    global deck
+def deal2player():
+    global deck, p1Hand, p2Hand
     url = "http://deckofcardsapi.com/api/deck/" + deck[0] + "/draw/?count=12"
     response = requests.get(url)
     
-    p1Hand = [None] * 6
-    p2Hand = [None] * 6
-    
-    for x in range(0,6):
+    for x in range(0,5):
+        print x
         img = response.json()['cards'][x]['image']
         code = response.json()['cards'][x]['code']
         value = response.json()['cards'][x]['value']
         suit = response.json()['cards'][x]['suit']
         p1Hand[x] = Card(img, code, value, suit)
         
-    for p in range(0,6):
-        p1Hand[p].getValue()
+    for x in range(6,12):
+        print x
+        img = response.json()['cards'][x]['image']
+        code = response.json()['cards'][x]['code']
+        value = response.json()['cards'][x]['value']
+        suit = response.json()['cards'][x]['suit']
+        p2Hand[x-6] = Card(img, code, value, suit)
         
    
 getDeck()
-dealhandplayer()
+deal2player()
